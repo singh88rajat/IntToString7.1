fc8c44e65d412918182762a3ec6bc95eadf3b1af
Mukesh Kumar <singh88rajat@gmail.com>
2017-02-10 12:37:38 +0530

    IntToString7.1

---
+++ src/IntToString.java
@@ -0,0 +1,13 @@
+
+public class IntToString {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		
+		//String b = String.valueOf(a);  //This is also an inbuilt method to convert int to string
+		
+		String b = Integer.toString(a);
+		System.out.println("The converted values is : "+b);
+
+	}
+}
